3aaf311289606b2f5866fd7f5d878c875d5625c9
mayurikapadnis <mayurikapadnis738@gmail.com>
2026-07-17 23:26:57 +0530

    initial commit ep-01

---
+++ app.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+]);
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
