24b9538d46d8a46a73ad6f3df948383256096673
mayurikapadnis <mayurikapadnis738@gmail.com>
2026-08-01 13:34:45 +0530

    second episode done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1"),
-    React.createElement("h2", {}, "h2"),
+    React.createElement("h1", {}, "mayuri h1"),
+    React.createElement("h2", {}, "mayuri h2"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "h1"),
@@ -9,6 +12,5 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
